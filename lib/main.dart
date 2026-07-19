9c93d16856fad651ff155bd6f03e4e8134216466
sera-liz <seralizdavid@gmail.com>
2026-07-19 20:11:22 +0530

    Rename screenshots and improve README

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+  
       title: 'E-Commerce UI',
       theme: ThemeData(
-        
+
         primarySwatch: Colors.purple,
       ),
       
